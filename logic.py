41948279570af0ed4dc1310884a6e29ec84a036a
Hashim <hashbukhtiar@gmail.com>
2023-12-24 17:10:20 -0500

    Added functions to parse and format course listings text and write it to the course_listings.txt file

---
+++ logic.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+def update_class_data(filename):
+    # (str, str) -> None
+    # Takes a filename and URL and updates the course listings file with the page content
+    
+    print("Updating course listings...")
+    print("Clearing file...")
+    with open(filename, "w") as f:  pass # clear the file
+
+    num_page = 1
+    URL = f"https://academiccalendars.romcmaster.ca/content.php?catoid=53&catoid=53&navoid=10775&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D={num_page}#acalog_template_course_filter"
+
+    print("Parsing pages...")
+    while True:
+        response = requests.get(URL)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
+
+        page_content = format_cl_page_content(text)
+
+        if page_content == ['']:
+            break
+
+        num_page += 1
+        URL = f"https://academiccalendars.romcmaster.ca/content.php?catoid=53&catoid=53&navoid=10775&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D={num_page}#acalog_template_course_filter"
+
+        with open(filename, "a", encoding='utf-8') as f:
+            for line in page_content:
+                f.write(line + "\n")
+    
+    print(f"File updated, {num_page-1} pages parsed.")
+
+def format_cl_page_content(content):
+    # (str) -> list
+    # Takes a string of the page content and returns a list of the lines
+    
+    formatted_lines = []
+    content = content.splitlines()
+
+    for line in content:
+        if "•" in line:
+            formatted_line = line.replace("�", "").split()
+            formatted_lines.append(" ".join(formatted_line[1:]))
+
+    return formatted_lines
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from logic import *
+import gui
+
 def main():
-    print("Hello, World!")
+    MCMASTER_COURSE_LISTINGS = "https://academiccalendars.romcmaster.ca/content.php?catoid=53&navoid=10775"
+    COURSE_LISTINGS_FILENAME = "course_listings.txt"
+
+    #update_class_data(COURSE_LISTINGS_FILENAME) # uncomment to update course listings
+
 
 if __name__ == '__main__':
     main()
